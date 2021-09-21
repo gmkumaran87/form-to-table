99b147601035f4d7dac2d2a87f82ed3c5352ec3b
Muthukumaran <gmkumaran87@gmail.com>
2021-09-21 17:32:32 +0530

    Added the validation changes

--- index.js
+++ index.js
@@ -31,25 +31,59 @@ class UI {
 
         list.appendChild(row);
     }
+
+    diplayAlert(msg, flag) {
+        const validate = document.querySelector(".alert");
+        //  const container = document.querySelector(".container");
+
+        console.log(msg, flag);
+        validate.classList.toggle(flag);
+        validate.innerHTML = `${msg}`;
+
+        setTimeout(() => {
+            validate.innerHTML = "";
+            validate.classList.remove(flag);
+        }, 1500);
+    }
+    clearfields() {
+        fname.value = "";
+        lname.value = "";
+        address.value = "";
+        pincode.value = "";
+        state.value = "";
+        country.value = "";
+
+        gender.forEach((el) => {
+            if (el.checked) {
+                el.checked = false;
+            }
+        });
+        foods.forEach((el) => {
+            if (el.checked) {
+                el.checked = false;
+            }
+        });
+    }
 }
+
+// Global values which will be used
+const fname = document.getElementById("firstName");
+const lname = document.getElementById("lastName");
+const address = document.getElementById("address");
+
+const pincode = document.getElementById("pincode");
+const state = document.getElementById("state");
+const country = document.getElementById("country");
+
+// Radio and checkbox items
+const gender = document.getElementsByName("gender");
+const foods = document.getElementsByName("food");
 
 const handleSubmit = (e) => {
     e.preventDefault();
 
     let foodsChecked = [];
     let genderSelected = "";
-
-    const fname = document.getElementById("firstName").value;
-    const lname = document.getElementById("lastName").value;
-    const address = document.getElementById("address").value;
-
-    const pincode = document.getElementById("pincode").value;
-    const state = document.getElementById("state").value;
-    const country = document.getElementById("country").value;
-
-    // Radio and checkbox items
-    const gender = document.getElementsByName("gender");
-    const foods = document.getElementsByName("food");
 
     gender.forEach((el) => {
         if (el.checked) {
@@ -62,23 +96,13 @@ const handleSubmit = (e) => {
         }
     });
 
-    console.log(
-        fname,
-        lname,
-        pincode,
-        state,
-        country,
-        genderSelected,
-        foodsChecked
-    );
-
     const employee = new Employee(
-        fname,
-        lname,
-        address,
-        pincode,
-        state,
-        country,
+        fname.value,
+        lname.value,
+        address.value,
+        pincode.value,
+        state.value,
+        country.value,
         genderSelected,
         foodsChecked
     );
@@ -86,7 +110,17 @@ const handleSubmit = (e) => {
     // Inserting the inputted values in the Table
     const ui = new UI();
 
-    ui.addEmployee(employee);
+    if (fname.value && lname.value) {
+        ui.addEmployee(employee);
+        ui.diplayAlert("Record has been added to the table", "success");
+        // Clearing the form fields
+        ui.clearfields();
+    } else {
+        ui.diplayAlert(
+            "Please enter the name fields for adding the record",
+            "failure"
+        );
+    }
 };
 const btn = document.getElementById("submit-btn");
 
